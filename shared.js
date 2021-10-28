e8274b49b9d58cdbf5ec9200109f70ccdb82af81
neetesh24 <tamrakarnitesh21@gmail.com>
2021-10-28 13:24:11 +0545

    137.Opening and closing the hamburger menu

--- shared.js
+++ shared.js
@@ -6,8 +6,10 @@ var backdrop=document.querySelector('.backdrop');//doesn't have to be same as na
 
 // Adding the event listener
 var modal=document.querySelector('.modal');
+var modalNoButton=document.querySelector(".modal__action--negative")
 var selectPlanButtons=document.querySelectorAll('.plan button');
-
+var toggleButton=document.querySelector('.toggle-button');
+var mobileNav=document.querySelector('.mobile-nav');
 
 // adding event listener to these buttons
 for(var i = 0; i < selectPlanButtons.length ;i++){
@@ -17,3 +19,20 @@ for(var i = 0; i < selectPlanButtons.length ;i++){
 
     });
 }
+
+backdrop.addEventListener("click",function(){
+    mobileNav.style.display='none';
+    closeModal();
+});
+
+modalNoButton.addEventListener("click",closeModal);
+
+function closeModal(){
+    backdrop.style.display="none";
+    modal.style.display="none";
+}
+
+toggleButton.addEventListener('click',function(){
+    mobileNav.style.display='block';
+    backdrop.style.display='block';
+})
