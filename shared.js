a472187e8dc63bc1301a9942ef92b97c3bb0a607
neetesh24 <tamrakarnitesh21@gmail.com>
2021-10-28 12:19:39 +0545

    Adding Event Listener

--- shared.js
+++ shared.js
@@ -1,4 +1,19 @@
 var backdrop=document.querySelector('.backdrop');//doesn't have to be same as name of class
 
-// overwrites the css display style
+
+//--------------- overwrites the css display style
 // backdrop.style.display = 'block';
+
+// Adding the event listener
+var modal=document.querySelector('.modal');
+var selectPlanButtons=document.querySelectorAll('.plan button');
+
+
+// adding event listener to these buttons
+for(var i = 0; i < selectPlanButtons.length ;i++){
+    selectPlanButtons[i].addEventListener('click', function(){
+        modal.style.display='block';
+        backdrop.style.display='block';
+
+    });
+}
